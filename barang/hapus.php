a14464574f3bf1b67fa99d56f348f4a52f4ff282
Akbar Umar Alfaro <akbarumar88@gmail.com>
2022-04-06 11:39:27 +0700

    - Berhasil Hapus Data Barang

--- barang/hapus.php
+++ barang/hapus.php
@@ -7,27 +7,21 @@ require_once("../helper/function.php");
 $id = $_POST['id'];
 
 try {
-    //code...
-    // Select data terlebih dahulu (Pengecekan constraint).
-    $sql = "SELECT id FROM barang where idkategori=$id";
-    $barang = $conn
-        ->query($sql)
-        ->fetch();
     // dd($barang);
-    if (!empty($barang)) {
-        flash('kategori', 'Tidak dapat menghapus data kategori, data kategori sudah terpakai', 'error');    
-    } else {
-        // Aman, hapus data kategori
-        $sql = "DELETE FROM kategori where id=:id";
-        $cmd = $conn->prepare($sql);
-        $cmd->bindParam("id", $id);
-        $cmd->execute();
-        flash('kategori', 'Berhasil menghapus data kategori', 'success');
-    }
+    // if (!empty($barang)) {
+    //     flash('barang', 'Tidak dapat menghapus data barang, data barang sudah terpakai', 'error');    
+    // } else {
+    //     // Aman, hapus data barang
+    // }
+    $sql = "DELETE FROM barang where id=:id";
+    $cmd = $conn->prepare($sql);
+    $cmd->bindParam("id", $id);
+    $cmd->execute();
+    flash('barang', 'Berhasil menghapus data barang', 'success');
 
 } catch (Exception $e) {
-    flash('kategori', 'Terjadi kesalahan saat menghapus data kategori ' . $e->getMessage(), 'error');
+    flash('barang', 'Terjadi kesalahan saat menghapus data barang ' . $e->getMessage(), 'error');
 }
-header("location: /kategori/index.php");
+header("location: /barang/index.php");
 
 ?>
